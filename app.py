a5a807f24583f9358a57642717a2c5e01fddfaef
datachalet <139548802+datachalet@users.noreply.github.com>
2023-11-08 18:45:33 +0100

    major changes and detailexplorer

--- app.py
+++ app.py
@@ -135,11 +135,22 @@ elif selected_currency_option == 'CHF':
 #### Calculate Inflatiom ####
 #############################
 
+# def read_inflation_data(file_path, sheet_name, last_valid_entry, level):
+#     # Read the Excel file with the specified header row
+#     data = pd.read_excel(file_path, sheet_name=sheet_name, header=3)
+#     df = data.iloc[:last_valid_entry + 1]  # This will keep rows up to the last_valid_entry
+#     df = df[df["Level"] == level]  # Select Level in inflation data (default 2)
+
+#     return df
+
 def read_inflation_data(file_path, sheet_name, last_valid_entry, level):
     # Read the Excel file with the specified header row
     data = pd.read_excel(file_path, sheet_name=sheet_name, header=3)
     df = data.iloc[:last_valid_entry + 1]  # This will keep rows up to the last_valid_entry
-    df = df[df["Level"] == level]  # Select Level in inflation data (default 2)
+    if level is not None:
+        df = df[df["Level"] == level]  # Select Level in inflation data (default 2)
+    else:
+        df = df[df["Level"] != 1]  # Select Level in inflation data (default 2)
 
     return df
 
@@ -199,7 +210,10 @@ df_inflation_cumulative_l1 = calculate_cumulative_inflation(df_inflation_exchang
 
 # Reshape the data into long format
 df_inflation_cumulative_l2_long = pd.melt(df_inflation_cumulative_l2 , id_vars=['ID', 'PosTxt_E'], var_name='Year', value_name='Value')
-df_inflation_cumulative_l1_long = pd.melt(df_inflation_cumulative_l1, id_vars=['ID', 'PosTxt_E'], var_name='Year', value_name='Value')
+df_inflation_cumulative_l1_long = pd.melt(df_inflation_cumulative_l1 , id_vars=['ID', 'PosTxt_E'], var_name='Year', value_name='Value')
+
+cumulative_inflation_end = round(df_inflation_cumulative_l1_long.loc[df_inflation_cumulative_l1_long['Year'] == 2022, 'Value'].values[0], 1)
+
 
 # Line chart using Plotly in the first column
 fig_line = px.line(df_inflation_cumulative_l1_long,
@@ -219,14 +233,22 @@ st.plotly_chart(fig_line,
                 use_container_width=True,
                 auto_open=False)
 
-st.caption(f"Abbildung 1: Swiss Inflation from {start_year} - {end_year} denominated in {selected_currency_option}")
+st.caption(f"Figure 1: Swiss Inflation from {selected_year} - {end_year} denominated in {selected_currency_option}")
 
+
+st.write(f"Between {selected_year} and {end_year} the cumulative rate of inflation in Switzerland was {cumulative_inflation_end}% denominated in {selected_currency_option}. ")
+
+
+
+# Create a sorted legend DataFrame
+legend_df = df_inflation_cumulative_l2_long[['PosTxt_E']].drop_duplicates().sort_values(by='PosTxt_E')
 
 # Line chart using Plotly in the first column
 fig_line = px.line(df_inflation_cumulative_l2_long,
                 x='Year',
                 y='Value',  # Pass both indicators as a list
                 color='PosTxt_E',
+                category_orders={'PosTxt_E': legend_df['PosTxt_E']},
                 title="",
                 line_shape=line_shape,
                 color_discrete_sequence=custom_color_sequence)  # Add colors for each indicator
@@ -241,28 +263,28 @@ st.plotly_chart(fig_line,
                 use_container_width=True,
                 auto_open=False)
 
-st.caption(f"Abbildung 2: Swiss Inflation by Categories from {start_year} - {end_year} denominated in {selected_currency_option}")
+st.caption(f"Figure 2: Swiss Inflation by Categories from {start_year} - {end_year} denominated in {selected_currency_option}")
 
 
-### Dataframe with line ####
-df_development = df_inflation_cumulative_l2
-df_development ["Development"] = df_development .apply(lambda row: row[2:].tolist(), axis=1)
-df_development = df_development [["PosTxt_E","Development",2022]]
-df_development.reset_index(drop=True, inplace=True)
+# ### Dataframe with line ####
+# df_development = df_inflation_cumulative_l2
+# df_development ["Development"] = df_development .apply(lambda row: row[2:].tolist(), axis=1)
+# df_development = df_development [["PosTxt_E","Development",2022]]
+# df_development.reset_index(drop=True, inplace=True)
 
-st.dataframe(
-    df_development,
-    column_config={
-        "PosTxt_E": "Category",
-        "Development": st.column_config.LineChartColumn(
-            "Cumulative rate of inflation in %",
-            width="medium",
-            help="Cumulative rate of inflation in %"),
-        "2022":"Total rate of inflation in %"
-    },
-    hide_index=True,
-    use_container_width = True
-)
+# st.dataframe(
+#     df_development,
+#     column_config={
+#         "PosTxt_E": "Category",
+#         "Development": st.column_config.LineChartColumn(
+#             "Cumulative rate of inflation in %",
+#             width="medium",
+#             help="Cumulative rate of inflation in %"),
+#         "2022":"Total rate of inflation in %"
+#     },
+#     hide_index=True,
+#     use_container_width = True
+# )
 
 #########################
 ##### Basekt Weights ####
@@ -316,36 +338,178 @@ left_merged_df = pd.merge(left_merged_df, df_b_l2_10, on='ID', how='left')
 left_merged_df = pd.merge(left_merged_df, df_b_l2_15, on='ID', how='left')
 left_merged_df = pd.merge(left_merged_df, df_b_l2_20, on='ID', how='left')
 left_merged_df.columns = left_merged_df.columns.astype(str)
-df_weights = left_merged_df[['Position','ID','2000/01','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022','2023']]
+df_weights = left_merged_df[['PosTxt_E_x','ID','2000/01','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021','2022','2023']]
+
 df_weights = df_weights.rename(columns={'2000/01': '2001'})
 columns_to_convert = df_weights .columns[2:]  # Assuming the percentage columns start from the 3rd column
 df_weights[columns_to_convert] = df_weights [columns_to_convert] / 100 # convert to decimal
 
 
-selected_year = st.selectbox("Select a Year", df_weights.columns[2:])
+#selected_year = st.selectbox("Select a Year", df_weights.columns[2:])
+selected_year = '2022'
 
 # Filter data for the selected year
-year_data = df_weights[['Position', selected_year]]
+year_data = df_weights[['PosTxt_E_x', selected_year]]
 
 # Sort the DataFrame by the selected year in descending order
-year_data = year_data.sort_values(by=selected_year, ascending=False)
+year_data = year_data.sort_values(by='PosTxt_E_x', ascending=True)
 
-    # Donut Chart
-fig_donut = px.pie(
+# Bar Chart
+fig_bar = px.bar(
     year_data,
-    names='Position',
-    values=selected_year,
-    hole=0.5,
-    color_discrete_sequence=custom_color_sequence
+    x='PosTxt_E_x',
+    y=selected_year,
+    color='PosTxt_E_x',
+    color_discrete_sequence=custom_color_sequence,
+    labels={'PosTxt_E_x': 'Kategorie', selected_year: 'Weighting according to product basket in 2022'},
 )
 
-fig_donut.update_traces(textposition='inside', textinfo='percent')
-fig_donut.update_layout(
-    legend_title='Kategorie'
+st.plotly_chart(fig_bar)
+
+st.caption(f"Figure 3: Weighting according to swiss product basket in 2022")
+
+st.divider()
+st.header('Explore Specific Categories')
+################################
+####### detailed Products ######
+#################################
+
+# Example usage with chf_euro DataFrame
+file_path = 'data/su-e-05.02.67.xlsx'
+sheet_name = 'VAR_y-1'
+last_valid_entry = 415
+
+
+
+df_inflation_one_category = read_inflation_data(file_path,
+                                                sheet_name,
+                                                last_valid_entry,
+                                                level=None)
+
+
+def apply_exchange_rate_conversion_one_category(df, selected_category, exchange_rate_df):
+    """
+    Clean and convert the DataFrame based on the selected product and apply exchange rate conversion.
+
+    Args:
+        df_inflation (pd.DataFrame): The input DataFrame.
+        selected_product (str): The product to select from the 'PosTxt_E' column.
+        columns_to_drop (list): List of column names to drop.
+        value_to_drop (str): The value to drop from the DataFrame.
+        exchange_rate_df (pd.DataFrame): DataFrame with exchange rate data.
+
+    Returns:
+        pd.DataFrame: The cleaned and converted DataFrame.
+    """
+    # Filter the DataFrame based on the selected product
+    filtered_df = df[df['PosTxt_E'] == selected_category]
+
+    # Drop specified columns
+    filtered_df = filtered_df.drop(columns=['Code', 'COICOP', 'PosType', 'Level', 'Position_D', 'PosTxt_D', 'Position_F', 'PosTxt_F', 'Posizione_I', 'PosTxt_I', 'Item_E', '2023', 1983])
+
+    # Create a boolean mask for columns with the specified value
+    mask = (filtered_df == '...').any()
+
+    # Use the boolean mask to drop the columns
+    filtered_df = filtered_df.loc[:, ~mask]
+
+    # Check if exchange_rate_df is provided
+    if exchange_rate_df is not None:
+        # Get the list of columns to adjust
+        cols_to_adjust = filtered_df.columns[2:]
+
+        # Generate a list with years where exchange rate is available
+        known_exchange_years = list(exchange_rate_df['Year'])
+        known_exchange_years= known_exchange_years[1:] #Nessecary because some exchange rates dont go back till 1983
+
+        # Filter cols_to_adjust to only include years that are in known_exchange_years
+        cols_to_adjust = [col for col in cols_to_adjust if int(col) in known_exchange_years]
+        available_years = cols_to_adjust
+
+        # Create a list of columns to keep, including the 'PosTxt_E' column
+        columns_to_keep = ['PosTxt_E','PosNo'] + cols_to_adjust
+
+        # Filter the DataFrame based on the selected columns
+        filtered_df = filtered_df[columns_to_keep]
+
+        # Create a dictionary from exchange rate DataFrame
+        exchange_rate_change_dict = exchange_rate_df.set_index('Year')['Exchange Rate Change'].to_dict()
+
+        # Adjust the values in df based on the exchange rate change
+        for col in cols_to_adjust:
+            year = int(col)
+            filtered_df[col] = filtered_df[col] + (exchange_rate_change_dict.get(year, 0) * 100)
+
+    return filtered_df, available_years
+
+
+
+
+def calculate_cumulative_inflation_one_category(df, selected_year, last_year):
+    # Cumulative Inflation Logic
+    # Set the dynamic year values to 100
+    df.insert(loc = 2, column = selected_year, value = 100)
+
+    # Loop through years and calculate real values and subtract 100, starting from the dynamic year
+    for year in range(selected_year + 1, last_year + 1):
+        df[year] = df[year - 1] * (1 + df[year] / 100)
+
+    for year in range(selected_year, last_year + 1):
+        df[year] = df[year] - 100
+
+    subset_columns = ['PosTxt_E', 'PosNo', selected_year] + list(range(selected_year + 1, last_year + 1))
+    df = df[subset_columns]
+    df = df.rename(columns={'PosNo': 'ID'})
+
+    return df
+
+# Your list of options
+options = sorted(list(df_inflation_one_category['PosTxt_E']))
+
+# Create the selectbox with the filtered options
+selected_option = st.selectbox("Select a Category", options)
+
+df_inflation_exchange_adjusted_one_category, available_years = apply_exchange_rate_conversion_one_category(df =df_inflation_one_category,
+                                                                                                           selected_category = selected_option,
+                                                                                                           exchange_rate_df=exchange_rate_df)
+
+
+
+last_possible_year = available_years[-1]
+first_possible_year = available_years[0]
+selected_year = first_possible_year -1
+
+
+df_inflation_cumulative_one_category = calculate_cumulative_inflation_one_category(df=df_inflation_exchange_adjusted_one_category,
+                                                                                   selected_year=selected_year,
+                                                                                   last_year=last_possible_year)
+
+df_inflation_cumulative_one_category_long = pd.melt(df_inflation_cumulative_one_category, id_vars=['ID', 'PosTxt_E'], var_name='Year', value_name='Value')
+
+
+# Line chart using Plotly in the first column
+fig_line = px.line(df_inflation_cumulative_one_category_long,
+                x='Year',
+                y='Value',  # Pass both indicators as a list
+                title="",
+                color='PosTxt_E',
+                line_shape=line_shape,
+                color_discrete_sequence=custom_color_sequence)  # Add colors for each indicator
+
+fig_line.update_layout(
+    xaxis_title='',  # Hide the title of the x-axis
+    yaxis_title='Cumulative rate of inflation in %',
+    legend_title_text=''  # Hide the title of the x-axis
 )
 
-# Display the pie chart in the Streamlit app
-st.plotly_chart(fig_donut)
+st.plotly_chart(fig_line,
+                use_container_width=True,
+                auto_open=False)
+
+st.caption(f"Figure 1: Swiss Inflation for {selected_option} from {selected_year} - {last_possible_year} denominated in {selected_currency_option}")
+
+
+
     
 
 
